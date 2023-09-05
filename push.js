7d0ed391def1a52ebc0b01e4a0b15f7e50c40316
Ramesh Mahato <rameshmahato821@gmail.com>
2023-09-05 08:21:33 +0545

    first commit

---
+++ push.js
@@ -0,0 +1,11 @@
+//  push() methods add the elements in the last index respectively
+
+const fruits = ['apple', 'banana', 'mango', 'orange']
+
+fruits.push('kiwi')
+
+console.log(fruits)
+
+
+fruits.push('potato', 'onion')
+console.log(fruits)
