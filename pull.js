51c343a06af9f26f895fa42d45147c6fbff4a8ff
Ramesh Mahato <rameshmahato821@gmail.com>
2023-09-05 08:35:30 +0545

    adding all pop method

---
+++ pull.js
@@ -0,0 +1,8 @@
+
+// pop() method removes the last elements of an array
+const fruits = ['apple', 'banana', 'mango', 'orange']
+
+// removed itesms
+console.log(fruits.pop())
+// new array after removing item
+console.log(fruits)
